5e648b353ced9823e15309c81f569020e150e12c
Muhammad Saqib <muhammadsackib@gmail.com>
2024-09-08 19:46:22 +0500

    render an error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import { TopStories } from "./TopStories";
 
 export function App() {
@@ -6,10 +7,24 @@ export function App() {
     <>
       <nav className="bg-slate-300 min-h-8"></nav>
       <main className="mx-auto w-fit p-4 space-y-3">
-        <Suspense fallback={<p>Loading...</p>}>
-          <TopStories />
-        </Suspense>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<p>Loading...</p>}>
+            <TopStories />
+          </Suspense>
+        </ErrorBoundary>
       </main>
+    </>
+  );
+}
+
+function ErrorFallback({ error }: FallbackProps) {
+  return (
+    <>
+      <p>
+        Something went wrong while rendering the top stories, you can see the
+        error message below which could help you figure out what went wrong
+      </p>
+      <pre className="text-red-500">{error.message}</pre>
     </>
   );
 }
