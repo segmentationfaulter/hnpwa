e3ec99f4ff2d5d62a110c8887a2793adb433f1c4
Muhammad Saqib <muhammadsackib@gmail.com>
2024-09-13 10:35:09 +0500

    add nav items

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,12 +1,51 @@
-import { Outlet } from "react-router-dom";
+import clsx from "clsx";
+import { Outlet, NavLink, NavLinkRenderProps } from "react-router-dom";
+
+const textColor = "text-slate-600";
 
 export function AppLayout() {
   return (
     <>
-      <nav className="bg-slate-300 min-h-8"></nav>
-      <main className="container box-border mx-auto p-4 space-y-3">
+      <nav
+        className={clsx(
+          "flex",
+          "justify-center",
+          "gap-4",
+          "sm:gap-8",
+          "bg-slate-300",
+          "py-2",
+          textColor,
+        )}
+      >
+        <NavLink className={navLinkClasses} to="">
+          Top
+        </NavLink>
+        <NavLink className={navLinkClasses} to="newest">
+          New
+        </NavLink>
+        <NavLink className={navLinkClasses} to="ask">
+          Ask
+        </NavLink>
+        <NavLink className={navLinkClasses} to="show">
+          Show
+        </NavLink>
+        <NavLink className={navLinkClasses} to="jobs">
+          Jobs
+        </NavLink>
+      </nav>
+      <main
+        className={clsx("container", "mx-auto", "p-4", "space-y-3", textColor)}
+      >
         <Outlet />
       </main>
     </>
   );
+}
+
+function navLinkClasses({ isActive }: NavLinkRenderProps): string {
+  if (isActive) {
+    return "underline";
+  }
+
+  return "";
 }
