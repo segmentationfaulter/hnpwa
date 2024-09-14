4b2f3fb7709dcbc4b513bebb390630b0b3e811eb
Muhammad Saqib <muhammadsackib@gmail.com>
2024-09-14 17:43:49 +0500

    make things ready for prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,19 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
     clean: true,
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "/dist",
-    historyApiFallback: true,
-    hot: true,
   },
   module: {
     rules: [
